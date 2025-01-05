bba18fa6bb58e2ff79beba526e85f570058d7be7
Shlok Motwani <shlok.codes@gmail.com>
2025-01-05 15:21:59 +0530

    feat: color picker with hex value display

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
- 
+  const [hex, setHex] = useState("#000000");
+
+  function handleHexChange(e) {
+    setHex(e.target.value);
+  }
+  return (
+    <div>
+      <input type="color" value={hex} onChange={handleHexChange}/>
+      <p>Hex Value : 
+        <span>{hex}</span>
+        </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
